710e9915abef6cb369c5b0605755b0e24bd8f1ad
inaki.aranzadi@optimizely.com <inaki.aranzadi@optimizely.com>
2018-01-03 15:04:04 -0800

    Add OUI classes to smart-pages story

--- src/stories/smart-pages.story.js
+++ src/stories/smart-pages.story.js
@@ -45,138 +45,148 @@ stories
   ));
 
 stories
-  .add('default state', () => (<div>
-    <h2 className='beta'>New Page</h2>
-    <TextField label="Name" onChange={anonymFn} onBlur={anonymFn} onKeyDown={anonymFn} placeholder="Page name" type="text" />
-    <TextField label="Editor url" onChange={anonymFn} onBlur={anonymFn} onKeyDown={anonymFn} placeholder="URL" type="text" />
+  .add('default state', () => (<div style={ styles.container }>
+    <div className='push-quad--ends'><h2 className='beta'>New Page</h2></div>
+    <div className='push--bottom'><TextField label="Name" onChange={anonymFn} onBlur={anonymFn} onKeyDown={anonymFn} placeholder="Page name" type="text" /></div>
+    <div className='push-quad--bottom'><TextField label="Editor url" onChange={anonymFn} onBlur={anonymFn} onKeyDown={anonymFn} placeholder="URL" type="text" /></div>
+    <div className='push-quad--ends lego-grid' style={{ width: 550 }}>
+      <div className='lego-grid__cell width--4-8'>
+        <Dropdown fullWidth={ true } style='outline' buttonContent='Immediately' width={400}>
+          <BlockList>
+            { data && data.map((item, index) => <BlockList.Category header={ item.header } key={ index }>
+                    <BlockList.Item onClick={ anonymFn }>
+                      <div className="flex flex-align--center">
+                        <div className="flex--1">
+                          <div>{ item.title }</div>
+                          <div className="muted micro">{ item.description }</div>
+                        </div>
+                      </div>
+                    </BlockList.Item>
+                  </BlockList.Category>)
+            }
+          </BlockList>
+        </Dropdown>
+      </div>
+      <div className='lego-grid__cell width--1-8'>check if</div>
+      <div className='lego-grid__cell width--1-8'>  
+        <Dropdown fullWidth={ false } style='outline' buttonContent='any' width={400}>
+          <BlockList>
+            {
+              dataAny.map((item, index) => {
+                return (
+                  <BlockList.Category header={ item.header } key={ index }>
+                    <BlockList.Item onClick={ anonymFn }>
+                      <div className="flex flex-align--center">
+                        <div className="flex--1">
+                          <div>{ item.title }</div>
+                          <div className="muted micro">{ item.description }</div>
+                        </div>
+                      </div>
+                    </BlockList.Item>
+                  </BlockList.Category>
+                );
+              })
+            }
+          </BlockList>
+        </Dropdown>
+      </div>
+      <div className='lego-grid__cell width--2-8'>
+        conditions is valid.
+      </div>
+    </div>
 
-    <Dropdown fullWidth={ false } style='outline' buttonContent='Immediately' width={400}>
-      <BlockList>
-        { data && data.map((item, index) => <BlockList.Category header={ item.header } key={ index }>
-                <BlockList.Item onClick={ anonymFn }>
-                  <div className="flex flex-align--center">
-                    <div className="flex--1">
-                      <div>{ item.title }</div>
-                      <div className="muted micro">{ item.description }</div>
-                    </div>
-                  </div>
-                </BlockList.Item>
-              </BlockList.Category>)
-        }
-      </BlockList>
-    </Dropdown>
-    check if
-    <Dropdown
-      fullWidth={ false }
-      style='outline'
-      buttonContent='any'
-      width={400}>
-      <BlockList>
-        {
-          dataAny.map((item, index) => {
-            return (
-              <BlockList.Category header={ item.header } key={ index }>
-                <BlockList.Item onClick={ anonymFn }>
-                  <div className="flex flex-align--center">
-                    <div className="flex--1">
-                      <div>{ item.title }</div>
-                      <div className="muted micro">{ item.description }</div>
-                    </div>
-                  </div>
-                </BlockList.Item>
-              </BlockList.Category>
-            );
-          })
-        }
-      </BlockList>
-    </Dropdown>
-    conditions is valid.
-
-    <Card title="Card Title" onClose testSection="card">
-      URL 
-      <Select name="match" id="match">
+    <Card className='push-quad--ends lego-grid' title="Card Title" onClose testSection="card">
+      <span>URL</span>
+      <span className='push--sides'><Select name="match" id="match">
         <option value="one">
           does match
         </option>
         <option value="two">
           does not match
         </option>
-      </Select>
-      this <strong>URLs</strong>:
+      </Select></span>
+      this <span className='weight--bold'>URLs</span>:
+    
+      <div className='push-double--ends'><TextField onChange={anonymFn} onBlur={anonymFn} onKeyDown={anonymFn} placeholder="URL" type="text" /></div>
 
-      <TextField
-        onChange={anonymFn}
-        onBlur={anonymFn}
-        onKeyDown={anonymFn}
-        placeholder="URL"
-        type="text"
-      />
-
-      <Select name="simplematch" id="simplematch">
-        <option value="one">
-          simple match
-        </option>
-        <option value="two">
-          exact match
-        </option>
-        <option value="two">
-          substring match
-        </option>
-        <option value="two">
-          regular expression
-        </option>
-      </Select>
-      <Button>
-        <Icon name="add" size="large"/>
-      </Button>
+      <div className='cf'>
+        <div className='float--left'><Select name="simplematch" id="simplematch">
+          <option value="one">
+            simple match
+          </option>
+          <option value="two">
+            exact match
+          </option>
+          <option value="two">
+            substring match
+          </option>
+          <option value="two">
+            regular expression
+          </option>
+        </Select></div>
+        <div className='float--right push--top'><Button size='small'><Icon name="add" size="medium"/></Button></div>
+      </div>
     </Card>
     
-    <Dropdown
-      fullWidth={ true }
-      buttonContent='any'
-      width={'100%'}>
-      <BlockList>
-        {
-          dataAddButton.map((item, index) => {
-            return (
-              <BlockList.Category header={ item.header } key={ index }>
-                <BlockList.Item onClick={ anonymFn }>
-                  <div className="flex flex-align--center">
-                    <div className="flex--1">
-                      <div>{ item.title }</div>
-                      <div className="muted micro">{ item.description }</div>
+    <div className='push-triple--ends'>
+      <Dropdown fullWidth={ true } buttonContent='any' width={'100%'}>
+        <BlockList>
+          {
+            dataAddButton.map((item, index) => {
+              return (
+                <BlockList.Category header={ item.header } key={ index }>
+                  <BlockList.Item onClick={ anonymFn }>
+                    <div className="flex flex-align--center">
+                      <div className="flex--1">
+                        <div>{ item.title }</div>
+                        <div className="muted micro">{ item.description }</div>
+                      </div>
                     </div>
-                  </div>
-                </BlockList.Item>
-              </BlockList.Category>
-            );
-          })
-        }
-      </BlockList>
-    </Dropdown>
+                  </BlockList.Item>
+                </BlockList.Category>
+              );
+            })
+          }
+        </BlockList>
+      </Dropdown>
+    </div>
 
-    <Disclose childrenStyle='divider' title='Advanced'>
-      <ul className="push-half oui-input-list">
-        <li>
-          <Checkbox
-            label="When triggered, deactivate if conditions are not met"
-            testSection="configure-view-smart-deactivation_enabled"
-          />
-        </li>
-        <li className="push-double--left">
-          <Checkbox
-            label="Undo changes when page activates"
-            testSection="configure-view-smart-undo_on_deactivation"
-          />
-          <div className="push-triple--left muted">
-            Undoing changes during page activation allows your single page app
-            to dynamically apply and remove changes. Learn more about when to
-            use this, and why this might be useful.
-          </div>
-        </li>
-      </ul>
-    </Disclose>
-    <hr/>
-    <Button style='plain'>Cancel</Button>
-    <Button style='highlight'>Create Page</Button>
+    <div className='push-quad--ends'>
+      <Disclose childrenStyle='divider' title='Advanced'>
+        <ul className="push-half oui-input-list">
+          <li>
+            <Checkbox
+              label="When triggered, deactivate if conditions are not met"
+              testSection="configure-view-smart-deactivation_enabled"
+            />
+          </li>
+          <li className="push-double--left">
+            <Checkbox
+              label="Undo changes when page activates"
+              testSection="configure-view-smart-undo_on_deactivation"
+            />
+            <div className="push-triple--left muted">
+              Undoing changes during page activation allows your single page app
+              to dynamically apply and remove changes. Learn more about when to
+              use this, and why this might be useful.
+            </div>
+          </li>
+        </ul>
+      </Disclose>
+    </div>
+    
+    <div className='border--top'></div>
+    
+    <div className='float--right push-double--top'>
+      <Button style='plain'>Cancel</Button>
+      <span className='push-double--left'><Button style='highlight'>Create Page</Button></span>
+    </div>
   </div>));
+
+  const styles = {
+    constainer: {
+      display: 'grid',
+      gridTemplateColumns: '20% 20% 20% 20% 20%',
+      gridTemplateRows: '20% 20% 20% 20% 20%'
+    } 
+  }
