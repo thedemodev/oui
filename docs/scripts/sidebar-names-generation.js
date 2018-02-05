060101259e25379b5f08b4df57fb1be01d7d7bd2
inaki.aranzadi@optimizely.com <inaki.aranzadi@optimizely.com>
2018-02-05 04:57:23 -0800

    Fix relative paths and create & add task to yarn develop script

--- docs/scripts/sidebar-names-generation.js
+++ docs/scripts/sidebar-names-generation.js
@@ -1,10 +1,13 @@
+// this file will run everytime you run `yarn develop`
+// the server must be restarted anytime a file is added to the pages folders
+
 const fs = require('fs');
 const path = require('path');
-const writeFile = require('fs').writeFile;
 
 const filenames = [
-  './docs/src/pages/design/',
-  './docs/src/pages/components/'
+  path.resolve('src/pages/design/'),
+  path.resolve('src/pages/components/'),
+  path.resolve('src/data/')
 ];
 
 const read = (dir) =>
@@ -16,22 +19,21 @@ const read = (dir) =>
       []);
 
 const designNames = read(filenames[0])
-                  .map( file => file.split('/')[4].split('.')[0])
+                  .map( file => file.split('/')[9].split('.')[0])
                   .filter( name => name !== 'index');
 
 const componentNames = read(filenames[1])
-                  .map( file => file.split('/')[4].split('.')[0])
+                  .map( file => file.split('/')[9].split('.')[0])
                   .filter( name => name !== 'index');
 
-
 //Write components names for sidebar
-fs.writeFile('./docs/src/data/components-names.json', JSON.stringify({ components: componentNames }), (err) => {
+fs.writeFile(`${filenames[2]}/components-names.json`, JSON.stringify({ components: componentNames }), (err) => {
   if (err) throw err;
   console.log('components names have been saved in ./src/data/components-names.json!');
 });
 
-//Write names of design section pages for sidebar
-fs.writeFile('./docs/src/data/design-names.json', JSON.stringify({ designNames: [...new Set(designNames)] }), (err) => {
+// //Write names of design section pages for sidebar
+fs.writeFile(`${filenames[2]}/design-names.json`, JSON.stringify({ designNames: [...new Set(designNames)] }), (err) => {
   if (err) throw err;
   console.log('design dir folder names have been save in ./src/data/design-names.json!');
 });
