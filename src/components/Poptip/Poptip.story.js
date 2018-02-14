65a80eb6e7b5f5d79eea955239d5c8ac0716edba
Dave Rau <dave.rau@optimizely.com>
2018-02-14 12:10:11 -0800

    Remove unused select

--- src/components/Poptip/Poptip.story.js
+++ src/components/Poptip/Poptip.story.js
@@ -3,8 +3,6 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
 import { withInfo } from '@storybook/addon-info';
-import { select } from '@storybook/addon-knobs';
-
 
 import Poptip from './index.js';
 import Button from '../Button';
