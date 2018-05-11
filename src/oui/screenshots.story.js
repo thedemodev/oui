2077154062045fe8c5c1075d09baca55edbd3a22
Dave Rau <dave.rau@optimizely.com>
2018-05-11 13:22:20 -0700

    No mobile

--- src/oui/screenshots.story.js
+++ src/oui/screenshots.story.js
@@ -5,16 +5,10 @@ addDecorator(initScreenshot());
 // Add decorator to entire Storybook instance to grab all stories
 addDecorator(withScreenshot({
   // A one-second delay ensures that fonts load before screenshots are taken.
-  delay: 1000,
+  delay: 500,
   browserTimeout: 5000,
   // We take screenshots at multiple viewport sizes/media queries
   viewport: [
-    {
-      width: 320,
-      height: 568,
-      isMobile: true,
-      hasTouch: true,
-    },
     {
       width: 1280,
       height: 740,
