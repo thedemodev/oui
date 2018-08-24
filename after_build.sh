7ba063b9e35f4ea11a7d6d6499031c4337337cad
James Fox <james@optimizely.com>
2018-08-23 21:11:51 -0700

    more checks

--- after_build.sh
+++ after_build.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 
 # Only send after a pull request
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
@@ -5,6 +6,14 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     exit 0
 fi
 
-curl -X POST -H "Content-Type: application/json" -d \
-  '{"TRAVIS_PULL_REQUEST":"$TRAVIS_PULL_REQUEST"}' \
-  https://gzme9ljy0e.execute-api.us-west-2.amazonaws.com/devops/handleCustomWebhook
+curl -X POST https://gzme9ljy0e.execute-api.us-west-2.amazonaws.com/devops/handleCustomWebhook \
+-H "Content-Type: application/json" \
+-d @- << EOF
+
+{
+  "TRAVIS_PULL_REQUEST": $TRAVIS_PULL_REQUEST,
+  "TRAVIS_TEST_RESULT": $TRAVIS_TEST_RESULT,
+  "TRAVIS_BUILD_STAGE_NAME": $TRAVIS_BUILD_STAGE_NAME,
+  "TRAVIS_JOB_NUMBER": $TRAVIS_JOB_NUMBER
+}
+EOF
