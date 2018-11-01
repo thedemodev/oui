064800916cff19d162f6fad7f67a00078098da43
pauloptimizely <paul.serraino@optimizely.com>
2018-11-01 18:42:01 -0500

    FileUpload should render preview when a file is dropped

--- src/components/FileUpload/FileUpload.js
+++ src/components/FileUpload/FileUpload.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Dropzone from 'react-dropzone';
 
+import Icon from 'react-oui-icons';
+
 const MEGABYTE = 1000000;
+const ICON_TYPE = {
+  tif: 'image',
+  tiff: 'image',
+  gif: 'image',
+  jpeg: 'image',
+  jpg: 'image',
+  png: 'image',
+  doc: 'document',
+  docx: 'document',
+  txt: 'document',
+  pdf: 'pdf',
+  xls: 'spreadsheet',
+  xlsx: 'spreadsheet',
+  csv: 'spreadsheet',
+};
 
 /**
  * FileUpload
@@ -16,31 +33,106 @@ const MEGABYTE = 1000000;
  *  />
  */
 
-const FileUpload = (props) => {
-  const {
-    maxSize,
-    onDrop,
-    onReject,
-    testSection,
-  } = props;
+class FileUpload extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      preview: null,
+      fileName: '',
+      fileSize: 0,
+      iconType: '',
+    };
+  }
 
-  return (
-    <Dropzone
-      className='file-upload border--all'
-      activeClassName='file-upload--active'
-      onDropAccepted={ onDrop }
-      onDropRejected={ onReject }
-      maxSize={ maxSize * MEGABYTE }>
-      <div className='flex flex-align--center flex--column flex-justified--center push-half--top'>
+  handleDrop = (files) => {
+    const {
+      onDrop,
+    } = this.props;
 
-        <h1 data-test-section={ `${ testSection }-title` }>Drag and drop a file here</h1>
-        <p data-test-section={ `${ testSection }-subtitle` }>
-          Or click to select a file. Max file size: { maxSize }MB
-        </p>
+    const file = files[0];
+    const fileName = file.name || '';
+    const extension = fileName.split('.').pop().toLowerCase();
+
+    this.setState({
+      fileName,
+      fileSize: (file.size || 0) / MEGABYTE,
+      iconType: ICON_TYPE[extension] || 'file',
+      preview: URL.createObjectURL(file),
+    }, () => {
+      onDrop(files);
+    });
+  }
+
+  renderPreview() {
+    const { testSection } = this.props;
+    const {
+      fileSize,
+      iconType,
+      preview,
+    } = this.state;
+
+    if (iconType === 'image') {
+      return (<img src={ preview } data-test-section={ `${ testSection }-preview-image` } />);
+    }
+
+    return (
+      <div>
+        <Icon name={ iconType } size='medium' testSection={ `${ testSection }-preview-icon` } />
+        <div data-test-section={ `${ testSection }-preview-size` }>{ fileSize } MB</div>
       </div>
-    </Dropzone>
-  );
-};
+    );
+  }
+
+  render() {
+    const {
+      maxSize,
+      onReject,
+      testSection,
+    } = this.props;
+
+    const {
+      fileName,
+      preview,
+    } = this.state;
+
+    return (
+      <div>
+        {
+          preview && (
+            <p data-test-section={ `${ testSection }-preview-file-name` }>File: { fileName }</p>
+          )
+        }
+        <Dropzone
+          className='file-upload border--all'
+          activeClassName='file-upload--active'
+          onDropAccepted={ this.handleDrop }
+          onDropRejected={ onReject }
+          maxSize={ maxSize * MEGABYTE }
+          multiple={ false }>
+          <div className='flex flex-align--center flex--column flex-justified--center'>
+            {
+              preview ?
+                this.renderPreview() :
+                <React.Fragment>
+                  <h1 className='push-half--top' data-test-section={ `${ testSection }-title` }>Drag and drop a file here</h1>
+                  <p data-test-section={ `${ testSection }-subtitle` }>
+                    Or click to select a file. Max file size: { maxSize }MB
+                  </p>
+                </React.Fragment>
+            }
+          </div>
+        </Dropzone>
+        {
+          preview && (
+            <p data-test-section={ `${ testSection }-preview-info` }>
+              Drag and drop a new file onto the file preview or click on the preview to select a new file.
+            </p>
+          )
+        }
+      </div>
+    );
+  }
+}
 
 FileUpload.defaultProps = {
   maxSize: 200,
--- src/components/FileUpload/tests/index.js
+++ src/components/FileUpload/tests/index.js
@@ -4,11 +4,11 @@ import Dropzone from 'react-dropzone';
 
 import FileUpload from '../index';
 
-describe('components/FileUpload', function() {
+describe('components/FileUpload', () => {
   let component;
   let mockMaxSize;
   let testSection;
-  beforeEach(function() {
+  beforeEach(() => {
     mockMaxSize = 200; // mb
     testSection = 'file-upload';
     component = mount(
@@ -19,24 +19,24 @@ describe('components/FileUpload', function() {
     );
   });
 
-  afterEach(function() {
+  afterEach(() => {
     component.unmount();
   });
 
-  describe('basic rendering', function() {
-    it('should render Dropzone', function() {
+  describe('basic rendering', () => {
+    it('should render Dropzone', () => {
       const dropzoneComponent = component.find(Dropzone);
       expect(dropzoneComponent.exists()).toBe(true);
     });
 
-    it('should render title text', function() {
+    it('should render title text', () => {
       const titleText = 'Drag and drop a file here';
       const titleComponent = component.find(`[data-test-section="${ testSection }-title"]`);
       expect(titleComponent.exists()).toBe(true);
       expect(titleComponent.text()).toEqual(titleText);
     });
 
-    it('should render subtitle text', function() {
+    it('should render subtitle text', () => {
       const subtitleText = `Or click to select a file. Max file size: ${ mockMaxSize }MB`;
       const subtitleComponent = component.find(`[data-test-section="${ testSection }-subtitle"]`);
       expect(subtitleComponent.exists()).toBe(true);
@@ -44,4 +44,63 @@ describe('components/FileUpload', function() {
     });
 
   });
+
+  describe('preview rendering', () => {
+    global.URL = {
+      createObjectURL: (x) => x,
+    };
+    let mockFile;
+    let mockFileContent;
+    let mockFileName;
+    beforeEach(() => {
+      mockFileContent = '00000000000000';
+      mockFileName = 'foo.png';
+      mockFile = new File([mockFileContent], mockFileName, {
+        type: 'text/plain',
+      });
+      component.instance().handleDrop([ mockFile ]);
+      component.update();
+    });
+
+    it('should render preview info', () => {
+      const infoComponent = component.find(`${ testSection }-preview-info`);
+      const expectedText = 'Drag and drop a new file onto the file preview or click on the preview to select a new file.';
+      expect(infoComponent.exists()).toBe(true);
+      expect(infoComponent.text()).toEqual(expectedText);
+    });
+
+    it('should render file name', () => {
+      const fileNameComponent = component.find(`${ testSection }-preview-file-name`);
+      expect(fileNameComponent.exists()).toBe(true);
+      expect(fileNameComponent.text()).toEqual(`File: ${ mockFileName }`);
+    });
+
+    describe('image preview', () => {
+      it('should render preview image', () => {
+        const imageComponent = component.find(`${ testSection }-preview-image`);
+        expect(imageComponent.exists()).toBe(true);
+      });
+    });
+
+    describe('document preview', () => {
+      beforeEach(() => {
+        component.setState({
+          iconType: 'document',
+        });
+        component.update();
+      });
+
+      it('should render Icon', () => {
+        const iconComponent = component.find(`${ testSection}-preview-icon`);
+        expect(iconComponent.exists()).toBe(true);
+      });
+
+      it('should render file size', () => {
+        const fileSizeComponent = component.find(`${ testSection }-preview-size`);
+        expect(fileSizeComponent.exists()).toBe(true);
+        expect(fileSizeComponent.text()).toEqual(`${ component.state('fileSize')} MB`);
+      });
+    });
+  });
+
 });
