edcc6869db8482b68774b7668cbb8716752cf83b
Dave Rau <dave.rau@optimizely.com>
2019-03-28 10:36:12 -0700

    lint tests

--- src/tokens/formatters/javascript-cjs.test.js
+++ src/tokens/formatters/javascript-cjs.test.js
@@ -1,19 +1,21 @@
 const toCJS = require('./javascript-cjs');
 
-describe('throws an error when no `id` is provided', () => {
-  const token = {
-    value: 'hsla(227, 100%, 75%, 1)',
-    name: 'Brand Blue (Light)',
-  };
+describe('throws errors', () => {
+  it('throws an error when no `id` is provided', () => {
+    const token = {
+      value: 'hsla(227, 100%, 75%, 1)',
+      name: 'Brand Blue (Light)',
+    };
 
-  expect(() => toCJS('', token)).toThrow();
-});
+    expect(() => toCJS('', token)).toThrow();
+  });
 
-describe('throws an error when no `value` is provided', () => {
-  const token = {
-    id: 'brand-blue-light',
-    name: 'Brand Blue (Light)',
-  };
+  it('throws an error when no `value` is provided', () => {
+    const token = {
+      id: 'brand-blue-light',
+      name: 'Brand Blue (Light)',
+    };
 
-  expect(() => toCJS('', token)).toThrow();
+    expect(() => toCJS('', token)).toThrow();
+  });
 });
