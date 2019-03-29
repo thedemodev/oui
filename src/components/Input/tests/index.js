dd6883b80eac4ef05c5e24dc8d5c34c91f9e0966
zashraf1985 <35262377+zashraf1985@users.noreply.github.com>
2019-03-29 09:45:40 +0500

    patch(Input): Fixed the isRequired prop on <Input> component which wasn't working as expected. (#1136)
    # Summary
    isRequired prop on Input does not add `*` to the label . Fixed it to add a *.
    
    # Test Plan
    1. Thoroughly tested manually.
    2. Added a unit test to verify the isRequired prop functionality.

--- src/components/Input/tests/index.js
+++ src/components/Input/tests/index.js
@@ -209,6 +209,13 @@ describe('components/Input', () => {
     expect(console.error.calls.all()[0].args[0]).toContain('Must include a value for the label prop to use the isOptional prop'); // eslint-disable-line
   });
 
+  it('should render a label with * if isRequired is passed', () => {
+    const component = mount(
+      <Input type="text" testSection="foo" label="Input Label" isRequired={ true } />
+    );
+
+    expect(component.find('.oui-label--required').length).toBe(1);
+  });
 
   it('should not render a label by default', () => {
     const component = mount(
