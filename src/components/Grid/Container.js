58dd854fbd775170651e91f6659697306e194d1d
Dave Rau <dave.rau@optimizely.com>
2019-04-16 13:37:04 -0700

    WIP Grid component coming together

---
+++ src/components/Grid/Container.js
@@ -0,0 +1,43 @@
+import classNames from 'classnames';
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const propTypes = {
+  /**
+   * @default 'container'
+   */
+  bsPrefix: PropTypes.string,
+
+  /**
+   * Allow the Container to fill all of it's availble horizontal space.
+   */
+  fluid: PropTypes.bool,
+  /**
+   * You can use a custom element for this component
+   */
+  as: PropTypes.elementType,
+};
+
+const defaultProps = {
+  as: 'div',
+  fluid: false,
+};
+
+const Container = React.forwardRef(
+  ({ bsPrefix, fluid, as: Component, className, ...props }, ref) => {
+    const prefix = 'container';
+    return (
+      <Component
+        ref={ref}
+        {...props}
+        className={classNames(className, fluid ? `${prefix}-fluid` : prefix)}
+      />
+    );
+  },
+);
+
+Container.displayName = 'Container';
+Container.propTypes = propTypes;
+Container.defaultProps = defaultProps;
+
+export default Container;
