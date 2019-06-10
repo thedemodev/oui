10d03c917d8f3726a2386d6f520208a922900145
Derek Hammond <derek@optimizely.com>
2019-06-10 10:46:16 -0500

    feat(Dropdown): Allow render props with hide function (#1169)
    **Summary:**
    The goal of this PR is to fix a bug where the toggle function is always called when the Dropdown children elements are clicked, and also to also optionally allow a hide function to be used via the children using a [render props](https://reactjs.org/docs/render-props.html) function. The component will provide the children with the `handleHideChildren` function if the child has the type of function, otherwise it will render the them directly as a node. Unless we expect react to make nodes functions, this will be both backwards compatible and future safe.
    
    Pull this branch and run `yarn storybook` to play with the "Custom hide function" story.
    
    ![image](https://user-images.githubusercontent.com/7128855/59118546-11a7b480-8916-11e9-9d3c-578e9a970a2f.png)
    
    **Test plan:**
    - [x] Unit tests
    - [x] Manual testing

--- src/components/Dropdown/tests/index.js
+++ src/components/Dropdown/tests/index.js
@@ -155,4 +155,182 @@ describe('components/Dropdown', () => {
     );
     expect(shallowToJson(component)).toMatchSnapshot();
   });
+
+  it('should hide children when shouldHideChildrenOnClick is not passed and children are clicked', () => {
+    const component = mount(
+      <Dropdown
+        arrowIcon="down"
+        buttonContent={ <div data-test-section="dropdown-button">Press me!</div> }
+        placement="bottom-start"
+        style="outline">
+        <div data-test-section="dropdown-children">
+          <p data-test-section="dropdown-text">
+            This is some initial text! Click me, nothing happens.
+          </p>
+          <button data-test-section="dropdown-button-1">Doesn't Hide</button>,
+          <button data-test-section="dropdown-button-2">Hide Dropdown</button>,
+        </div>
+      </Dropdown>
+    );
+
+    expect(component.find('[data-test-section="dropdown-children"]').length).toBe(0);
+    component.find('[data-test-section="dropdown-button"]').simulate('click');
+    expect(component.find('[data-test-section="dropdown-children"]').length).toBe(1);
+    component.find('[data-test-section="dropdown-text"]').simulate('mouseover');
+    component.find('[data-test-section="dropdown-text"]').simulate('click');
+    expect(component.find('[data-test-section="dropdown-children"]').length).toBe(0);
+  });
+
+  it('should hide children when shouldHideChildrenOnClick is passed with true and children are clicked', () => {
+    const component = mount(
+      <Dropdown
+        arrowIcon="down"
+        buttonContent={ <div data-test-section="dropdown-button">Press me!</div> }
+        placement="bottom-start"
+        shouldHideChildrenOnClick={ true }
+        style="outline">
+        <div data-test-section="dropdown-children">
+          <p data-test-section="dropdown-text">
+            This is some initial text! Click me, nothing happens.
+          </p>
+          <button data-test-section="dropdown-button-1">Doesn't Hide</button>,
+          <button data-test-section="dropdown-button-2">Hide Dropdown</button>,
+        </div>
+      </Dropdown>
+    );
+
+    expect(component.find('[data-test-section="dropdown-children"]').length).toBe(0);
+    component.find('[data-test-section="dropdown-button"]').simulate('click');
+    expect(component.find('[data-test-section="dropdown-children"]').length).toBe(1);
+    component.find('[data-test-section="dropdown-text"]').simulate('mouseover');
+    component.find('[data-test-section="dropdown-text"]').simulate('click');
+    expect(component.find('[data-test-section="dropdown-children"]').length).toBe(0);
+  });
+
+  it('should NOT hide children when shouldHideChildrenOnClick is passed with false and children are clicked', () => {
+    const component = mount(
+      <Dropdown
+        arrowIcon="down"
+        buttonContent={ <div data-test-section="dropdown-button">Press me!</div> }
+        placement="bottom-start"
+        shouldHideChildrenOnClick={ false }
+        style="outline">
+        <div data-test-section="dropdown-children">
+          <p data-test-section="dropdown-text">
+            This is some initial text! Click me, nothing happens.
+          </p>
+          <button data-test-section="dropdown-button-1">Doesn't Hide</button>,
+          <button data-test-section="dropdown-button-2">Hide Dropdown</button>,
+        </div>
+      </Dropdown>
+    );
+
+    expect(component.find('[data-test-section="dropdown-children"]').length).toBe(0);
+    component.find('[data-test-section="dropdown-button"]').simulate('click');
+    expect(component.find('[data-test-section="dropdown-children"]').length).toBe(1);
+    component.find('[data-test-section="dropdown-text"]').simulate('mouseover');
+    component.find('[data-test-section="dropdown-text"]').simulate('click');
+    expect(component.find('[data-test-section="dropdown-children"]').length).toBe(1);
+    component.find('[data-test-section="dropdown-button-1"]').simulate('click');
+    expect(component.find('[data-test-section="dropdown-children"]').length).toBe(1);
+    component.find('[data-test-section="dropdown-button-2"]').simulate('click');
+    expect(component.find('[data-test-section="dropdown-children"]').length).toBe(1);
+  });
+
+  it('should only hide children after handleHideChildren is invoked when shouldHideChildrenOnClick is passed with false ' +
+     'and render props function child is provided', () => {
+    const component = mount(
+      <Dropdown
+        arrowIcon="down"
+        buttonContent={ <div data-test-section="dropdown-button">Press me!</div> }
+        placement="bottom-start"
+        shouldHideChildrenOnClick={ false }
+        style="outline">
+        {
+          ({ handleHideChildren }) => (
+            <div data-test-section="dropdown-children">
+              <p data-test-section="dropdown-text">
+                This is some initial text! Click me, nothing happens.
+              </p>
+              <button data-test-section="dropdown-button-1">Doesn't Hide</button>,
+              <button data-test-section="dropdown-button-2" onClick={ handleHideChildren }>Hide Dropdown</button>,
+            </div>
+          )
+        }
+      </Dropdown>
+    );
+
+    expect(component.find('[data-test-section="dropdown-children"]').length).toBe(0);
+    component.find('[data-test-section="dropdown-button"]').simulate('click');
+    expect(component.find('[data-test-section="dropdown-children"]').length).toBe(1);
+    component.find('[data-test-section="dropdown-text"]').simulate('mouseover');
+    component.find('[data-test-section="dropdown-text"]').simulate('click');
+    expect(component.find('[data-test-section="dropdown-children"]').length).toBe(1);
+    component.find('[data-test-section="dropdown-button-1"]').simulate('click');
+    expect(component.find('[data-test-section="dropdown-children"]').length).toBe(1);
+    component.find('[data-test-section="dropdown-button-2"]').simulate('click');
+    expect(component.find('[data-test-section="dropdown-children"]').length).toBe(0);
+  });
+
+  it('should hide children when clicked when shouldHideChildrenOnClick is passed with true ' +
+    'and render props function child is provided', () => {
+    const component = mount(
+      <Dropdown
+        arrowIcon="down"
+        buttonContent={ <div data-test-section="dropdown-button">Press me!</div> }
+        placement="bottom-start"
+        shouldHideChildrenOnClick={ true }
+        style="outline">
+        {
+          ({ handleHideChildren }) => (
+            <div data-test-section="dropdown-children">
+              <p data-test-section="dropdown-text">
+                This is some initial text! Click me, nothing happens.
+              </p>
+              <button data-test-section="dropdown-button-1">Doesn't Hide</button>,
+              <button data-test-section="dropdown-button-2" onClick={ handleHideChildren }>Hide Dropdown</button>,
+            </div>
+          )
+        }
+      </Dropdown>
+    );
+
+    expect(component.find('[data-test-section="dropdown-children"]').length).toBe(0);
+    component.find('[data-test-section="dropdown-button"]').simulate('click');
+    expect(component.find('[data-test-section="dropdown-children"]').length).toBe(1);
+    component.find('[data-test-section="dropdown-text"]').simulate('mouseover');
+    component.find('[data-test-section="dropdown-text"]').simulate('click');
+    expect(component.find('[data-test-section="dropdown-children"]').length).toBe(0);
+  });
+
+  it('should hide children after handleHideChildren is invoked when shouldHideChildrenOnClick is passed with true ' +
+    'and render props function child is provided', () => {
+    const component = mount(
+      <Dropdown
+        arrowIcon="down"
+        buttonContent={ <div data-test-section="dropdown-button">Press me!</div> }
+        placement="bottom-start"
+        shouldHideChildrenOnClick={ true }
+        style="outline">
+        {
+          ({ handleHideChildren }) => (
+            <div data-test-section="dropdown-children">
+              <p data-test-section="dropdown-text">
+                This is some initial text! Click me, nothing happens.
+              </p>
+              <button data-test-section="dropdown-button-1">Doesn't Hide</button>,
+              <button data-test-section="dropdown-button-2" onClick={ handleHideChildren }>Hide Dropdown</button>,
+            </div>
+          )
+        }
+      </Dropdown>
+    );
+
+    expect(component.find('[data-test-section="dropdown-children"]').length).toBe(0);
+    component.find('[data-test-section="dropdown-button"]').simulate('click');
+    expect(component.find('[data-test-section="dropdown-children"]').length).toBe(1);
+    component.find('[data-test-section="dropdown-button-2"]').simulate('mouseover');
+    component.find('[data-test-section="dropdown-button-2"]').simulate('click');
+    expect(component.find('[data-test-section="dropdown-children"]').length).toBe(0);
+  });
 });
