4158ce341758a70a5babf7f29c6396f9286208b5
Zeeshan Ashraf <zashraf@folio3.com>
2019-08-08 00:04:43 +0500

    Added Navbar header component. Need to figure out a proper way to handle css

---
+++ src/components/NavBar/Header/index.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+import Link from '../../Link';
+import Badge from '../../Badge';
+
+const NavBarHeader = (props) => {
+  const {
+    platformName,
+    projectName,
+    homeUrl,
+    showProjectName,
+    trialSectionBody,
+    isNavOpen,
+  } = props;
+  return (
+    <div data-test-section="navbar-header">
+      <li className="root-nav__logo push-double--bottom">
+        <Link href={ homeUrl }>
+          <div
+            className={ classNames({
+              'root-nav__logo--full': isNavOpen,
+              'root-nav__logo--mark': !isNavOpen,
+            }) }
+          />
+        </Link>
+      </li>
+      { trialSectionBody }
+      <li
+        className={ classNames({
+          'root-nav__project': true,
+          'root-nav-fader': !isNavOpen,
+        }) }>
+        { showProjectName && (
+          <div
+            className="epsilon truncate"
+            data-test-section="project-name">
+            { projectName }
+          </div>
+        ) }
+        <Badge color='primary'>
+          { platformName }
+        </Badge>
+      </li>
+    </div>
+  );
+};
+
+NavBarHeader.propTypes = {
+  /* Root Url for Logo Link */
+  homeUrl: PropTypes.string.isRequired,
+  /* Is Nav Open or collapsed */
+  isNavOpen: PropTypes.bool.isRequired,
+  /* Platform Name */
+  platformName: PropTypes.string.isRequired,
+  /* Project Name */
+  projectName: PropTypes.string.isRequired,
+  /* Boolean to show Project */
+  showProjectName: PropTypes.bool.isRequired,
+  /* Trial Section Content */
+  trialSectionBody: PropTypes.node,
+};
+
+NavBarHeader.defaultProps = {
+  trialSectionBody: null,
+};
+
+export default NavBarHeader;
