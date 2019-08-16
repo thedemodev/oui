3f76d7a458eac237d5baa3f46c166a6a41b46e03
Zeeshan Ashraf <zashraf@folio3.com>
2019-08-16 19:43:04 +0500

    Incorporated feedback

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -2,72 +2,117 @@ import React from 'react';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 
-import Header from './Header';
+import Link from '../Link';
+import Badge from '../Badge';
 import IconLink from './IconLink';
 
-const linksPropTypeShape = PropTypes.shape({
+const LINK = 'link';
+const PUSH_STATE = 'pushstate';
+const BUTTON = 'button';
+
+const linkPropTypes = {
+  /** Should show a separator line before this link. */
+  hasSeparator: PropTypes.bool,
+  /** Url to Navigate to when type is link. */
+  href: PropTypes.string,
+  /** Name of Icon. */
   iconName: PropTypes.string.isRequired,
+  /** Whether the link is highlighted blue as the current active nav link. */
   isActive: PropTypes.bool,
-  isVisible: PropTypes.bool.isRequired,
-  /* Object, { type: '', action: '', pushStateHandler: '' } */
-  /* type key value can be pushStateHref, linkHref, function */
-  /* if key is pushStateHref, action is string, handler is function */
-  /* if key is linkHref, action is string */
-  /* if key is function, action is function */
-  linkAction: PropTypes.object.isRequired,
-  linkDescription: PropTypes.string.isRequired,
-  /* String, name of test data section */
-  testSection: PropTypes.string.isRequired,
+  /** Condition in which this link is visible. */
+  isVisible: PropTypes.bool,
+  /** Description of url. */
+  linkDescription: PropTypes.string,
+  /** Handler called when link is clicked. */
+  onClick: PropTypes.func,
+  /** Name of test data section. */
+  testSection: PropTypes.string,
+  /** Type of Link. */
+  type: PropTypes.oneOf([LINK, PUSH_STATE, BUTTON]),
+};
 
-});
+const linkDefaultProps = {
+  href: '',
+  isActive: false,
+  isVisible: true,
+  linkDescription: '',
+  testSection: null,
+  hasSeparator: false,
+  onClick: () => {},
+  type: LINK,
+};
 
-const renderLinks = (links, isPrimary, isNavOpen) => (
-  <li className="push-double--ends">
-    <ul>
-      {
-        links.filter(item => item.isVisible).map((item => (
-          <IconLink
-            key={ `${item.iconName}-${item.linkDescription}` }
-            { ...item }
-            isSecondaryLink={ !isPrimary }
-            isNavOpen={ isNavOpen }
-          />
-        )))
-      }
-    </ul>
-  </li>
-);
+const PrimaryLink = props => <IconLink { ...props } isSecondaryLink={ false } />;
+PrimaryLink.propTypes = linkPropTypes;
+PrimaryLink.defaultProps = linkDefaultProps;
+
+const SecondaryLink = props => <IconLink { ...props } isSecondaryLink={ true } />;
+SecondaryLink.propTypes = linkPropTypes;
+SecondaryLink.defaultProps = linkDefaultProps;
+
+// TODO: Implement CurrentUser Component
+const CurrentUser = props => <div />;
+
+const renderPrimaryLinks = children => React.Children.toArray(children).filter(child => child.type === PrimaryLink);
+const renderSecondaryLinks = children => React.Children.toArray(children).filter(child => child.type === SecondaryLink);
+const renderCurrentUser = children => React.Children.toArray(children).filter(child => child.type === CurrentUser);
 
 const NavBar = (props) => {
   const {
-    isNavOpen,
+    isOpen,
     homeUrl,
     logo,
-    preTitle,
+    trialContent,
     title,
     badgeText,
-    primaryLinks,
-    secondaryLinks,
+    children,
   } = props;
   return (
     <ul
       className={ classNames({
         'root-nav': true,
-        'root-nav--open': isNavOpen,
+        'root-nav--open': isOpen,
       }) }
       data-test-section="p13n-root-navbar">
-      <Header
-        homeUrl={ homeUrl }
-        isNavOpen={ isNavOpen }
-        logo={ logo }
-        badgeText={ badgeText }
-        title={ title }
-        preTitle={ preTitle }
-      />
-      { renderLinks(primaryLinks, true, isNavOpen) }
+      <div data-test-section="navbar-header">
+        <li className="root-nav__logo push-double--bottom">
+          <Link href={ homeUrl }>
+            { logo }
+          </Link>
+        </li>
+        { trialContent }
+        <li
+          className={ classNames({
+            'root-nav__project': true,
+            'root-nav-fader': !isOpen,
+          }) }>
+          <div
+            className="epsilon truncate"
+            data-test-section="project-name">
+            { title }
+          </div>
+          <Badge color="primary">
+            { badgeText }
+          </Badge>
+        </li>
+      </div>
+      <li className="push-double--ends">
+        <ul>
+          { renderPrimaryLinks(children) }
+        </ul>
+      </li>
       <li className="anchor--bottom">
         <ul>
-          { renderLinks(secondaryLinks, false, isNavOpen) }
+          <li className="push-double--ends">
+            <ul>
+              { renderSecondaryLinks(children) }
+            </ul>
+          </li>
+          <li>
+            <ul>
+              { renderCurrentUser(children) }
+            </ul>
+          </li>
         </ul>
       </li>
     </ul>
@@ -77,31 +122,43 @@ const NavBar = (props) => {
 NavBar.propTypes = {
   /** Text to appear below title in a badge. */
   badgeText: PropTypes.string,
+  /* Allowed Children are <PrimaryLink>, <SecondaryLink>, <CurrentUser>. */
+  children: (props, propName) => {
+    const prop = props[propName];
+    let error = null;
+    React.Children.forEach(prop, (child) => {
+      if (![SecondaryLink, PrimaryLink, CurrentUser].includes(child.type)) {
+        error = new Error('Children should be of type PrimaryLink, SecondaryLink, or CurrentUser.');
+      }
+    });
+    if (!error && React.Children.toArray(prop).filter(child => child.type === CurrentUser).length > 1) {
+      error = new Error('There should be only one instance of `CurrentUser`');
+    }
+    return error;
+  },
   /** Url to navigate to when Brand Logo is clicked. */
   homeUrl: PropTypes.string,
   /** Is Navigation Bar open or closed. */
-  isNavOpen: PropTypes.bool,
+  isOpen: PropTypes.bool,
   /** Component containing brand logo. */
   logo: PropTypes.node,
-  /** Component to appear above title and below brand logo. */
-  preTitle: PropTypes.node,
-  /** Primary links of navigation bar. */
-  primaryLinks: PropTypes.arrayOf(linksPropTypeShape),
-  /** Primary links of navigation bar. */
-  secondaryLinks: PropTypes.arrayOf(linksPropTypeShape),
   /** Title of navigation bar */
   title: PropTypes.string,
+  /** Component to appear above title and below brand logo. */
+  trialContent: PropTypes.node,
 };
 
 NavBar.defaultProps = {
   badgeText: '',
   homeUrl: '',
-  isNavOpen: true,
+  isOpen: true,
   logo: null,
-  preTitle: null,
-  primaryLinks: [],
-  secondaryLinks: [],
   title: '',
+  trialContent: null,
 };
+
+NavBar.PrimaryLink = PrimaryLink;
+NavBar.SecondaryLink = SecondaryLink;
+NavBar.CurrentUser = CurrentUser;
 
 export default NavBar;
