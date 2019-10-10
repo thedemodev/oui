e920652106b24f9d79c67560077bc23294c138a3
Paul Serraino <41028823+pauloptimizely@users.noreply.github.com>
2019-10-10 14:29:19 -0500

    fix [DESIGN-630] TokensInput formatting (#1230)
    * fix [DESIGN-630] TokensInput formatting
    
    * fix TokensInput tests
    
    * update changelog
    
    * update (TokensInput) use atomic css classes

--- src/components/TokensInput/tests/index.js
+++ src/components/TokensInput/tests/index.js
@@ -67,7 +67,7 @@ describe('components/TokensInput', () => {
 
   describe('when rendering tokens with optional props', function() {
     describe('when optional prop is placeholder', function() {
-      const inputCSS = '.flex .flex--1 .min-width--150 .no-border .soft-half--ends .soft--sides';
+      const inputCSS = '.flex .flex--1 .min-width--150 .no-border .soft-half--ends .soft--sides .border-radius .push-half--left';
 
       beforeEach(function() {
         mockOnChange = jest.fn();
@@ -88,7 +88,7 @@ describe('components/TokensInput', () => {
     });
 
     describe('when optional prop is maxTags', function() {
-      const inputCSS = '.flex .flex--1 .no-border .soft-half--ends .soft--sides';
+      const inputCSS = '.flex .flex--1 .no-border .soft-half--ends .soft--sides .border-radius .push-half--left';
 
       beforeEach(function() {
         mockOnChange = jest.fn();
